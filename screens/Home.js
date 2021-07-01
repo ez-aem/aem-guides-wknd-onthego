d20f9951e8a591480912b055ebce40e28c1a5e17
Josh Zeltman <joshua.zeltman@gmail.com>
2021-07-01 14:33:21 -0300

    Added the Featured Tile Component from the original project
    Updated the Home screen to use the Featured Tile with hard-coded values

--- screens/Home.js
+++ screens/Home.js
@@ -8,7 +8,11 @@ export default function HomeRoute() {
   return (
     <SafeAreaView style={styles.safeContainer}>
       <View style={styles.container}>
-
+        <FeaturedTile
+          title="WKND Adventures"
+          imagePath="https://wknd.site/us/en/_jcr_content/root/container/carousel/item_1571954853062.coreimg.60.1600.jpeg/1622075943352/adobestock-216674449.jpeg"
+          navigation={() => alert("test")}
+        />
         <FeaturedTile
           title="Featured Adventure"
           subtitle="Downhill Skiing in Jackson Hole, Wyoming"
